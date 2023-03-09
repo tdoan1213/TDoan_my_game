b704ea6f8fb80c9545c8e3ed15f3f65cbe2b07dd
tdoan1213 <126900958+tdoan1213@users.noreply.github.com>
2023-03-09 12:02:11 -0800

    first

---
+++ My Game/settings.py
@@ -0,0 +1,17 @@
+#File created by Tim Doan
+
+# screen dimensions
+WIDTH = 800
+HEIGHT = 600
+
+# game settings
+FPS = 30
+RUNNING = True
+
+# player attributes
+PLAYER_ACC = 2
+PLAYER_FRICTION = 0.12
+
+# colors
+BLACK = (0,0,0)
+BLUE = (50,50,255)
